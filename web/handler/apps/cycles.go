4aaee630d7e3084f01322cd1d05f594b19319bb0
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-02-22 09:45:14 -0500

    added more functionality

---
+++ web/handler/apps/cycles.go
@@ -0,0 +1,77 @@
+package apps
+
+import (
+	"net/http"
+
+	"golang.org/x/net/context"
+)
+
+func createAppCycleHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
+
+func allAppCyclesHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
+
+func updateAppCycleHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
+
+func downloadAppCycleConfigHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
+
+func createAppCycleReleaseHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
+
+func updateAppCycleReleaseHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
+
+func uploadAppCycleReleaseHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
+
+func lockAppCycleReleaseHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
+
+func checkVersionAppCycleReleaseHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
+
+func downloadAppCycleReleaseHandler(
+	ctx context.Context,
+	w http.ResponseWriter,
+	r *http.Request) {
+
+}
--- web/handler/cycles/README.md
+++ web/handler/cycles/README.md
@@ -1,5 +1,46 @@
 # Cycles API
 
+
+## Create a cycle for an app
+
+POST  /apps/:appId/cycles
+GET   /apps/:appId/cycles
+PATCH /apps/:appId/cycles/:cycleId
+
+## Downloads the config file
+GET   /apps/:appId/cycles/:cycleId/config
+
+## Creates a new release
+POST  /apps/:appId/cycles/:cycleId/releases
+
+## Changes of release fields
+PATCH /apps/:appId/cycles/:cycleId/releases/:releaseId
+
+## Upload either apk or ipa
+POST  /apps/:appId/cycles/:cycleId/releases/:releaseId
+
+## Locked the version so client can download
+PATCH /apps/:appId/cycles/:cycleId/releases/:releaseId/lock
+
+## Check if any download is available
+GET   /apps/:appId/cycles/:cycleId/releases/:currentVersion
+
+{
+  "latest_version": "1.2.3"
+}
+
+## Download bundles
+
+POST /apps/:appId/cycles/:cycleId/releases/:version/download
+
+##
+
+## Get all cycles for specific app
+
+## Update cycles for specific app
+
+##
+
 POST      /cycles
 GET       /cycles
 PATCH     /cycles/:cycleId
