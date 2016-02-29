a4f2cfc8997d5107519897b27ce5ee177e0342cd
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-02-28 20:50:44 -0500

    added rsa crypto

--- lib/crypto/rsa_test.go
+++ lib/crypto/rsa_test.go
@@ -1,7 +1,59 @@
 package crypto_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/pressly/warpdrive/lib/crypto"
+)
+
+func TestParsePublicKey(t *testing.T) {
+	_, public, err := crypto.RSAKeyPair(1024)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = crypto.PublicKey(public)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestParsePrivateKey(t *testing.T) {
+	private, _, err := crypto.RSAKeyPair(1024)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = crypto.PrivateKey(private)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 func TestRSA(t *testing.T) {
+	private, public, err := crypto.RSAKeyPair(1024)
 
+	if err != nil {
+		t.Error(err)
+	}
+
+	message := "Hello This is Warpdrive."
+
+	encrypted, err := crypto.EncryptByPublicRSA([]byte(message), public, "sha256")
+	if err != nil {
+		t.Error(err)
+	}
+
+	decrypted, err := crypto.DecryptByPrivateRSA(encrypted, private, "sha256")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(decrypted) != message {
+		t.Errorf("got '%s' instead of '%s'", decrypted, message)
+	}
 }
