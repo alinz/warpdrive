5f8d082f0d295d6d3370431efd401acf5258b99f
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-03-02 22:04:54 -0500

    add logger middleware

--- web/web.go
+++ web/web.go
@@ -3,15 +3,18 @@ package web
 import (
 	"net/http"
 
+	"github.com/pressly/chi"
+	"github.com/pressly/chi/middleware"
+
 	"github.com/pressly/warpdrive/web/handler/apps"
 	"github.com/pressly/warpdrive/web/handler/session"
 	"github.com/pressly/warpdrive/web/handler/users"
-
-	"github.com/pressly/chi"
 )
 
 func New() http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
 
 	r.Mount("/session", session.Routes())
 	r.Mount("/users", users.Routes())
