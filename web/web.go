10db10cb5b1a709288464dc5dc3d9f0a1446441c
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-03-08 15:32:33 -0500

    added recover, ping, requestid and realip middleware

--- web/web.go
+++ web/web.go
@@ -3,6 +3,7 @@ package web
 import (
 	"net/http"
 
+	"github.com/goware/heartbeat"
 	"github.com/pressly/chi"
 	"github.com/pressly/chi/middleware"
 
@@ -14,7 +15,13 @@ import (
 func New() http.Handler {
 	r := chi.NewRouter()
 
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+
 	r.Use(middleware.Logger)
+
+	r.Use(heartbeat.Route("/ping"))
 
 	r.Mount("/session", session.Routes())
 	r.Mount("/users", users.Routes())
