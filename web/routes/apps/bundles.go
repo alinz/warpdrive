f86f3082c2c0041deca0421c83ef2bd3075866d6
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-11-22 11:36:48 -0500

    cleaned up code

---
+++ web/routes/apps/bundles.go
@@ -0,0 +1,11 @@
+package apps
+
+import "net/http"
+
+func uploadBundlesHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getBundlesHandler(w http.ResponseWriter, r *http.Request) {
+
+}
