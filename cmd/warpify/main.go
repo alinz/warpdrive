acbe008fe76b65a2bd66226ebd0aad29e28544ac
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-12-15 10:40:11 -0500

    added basic logic for ios and android

--- cmd/warpify/main.go
+++ cmd/warpify/main.go
@@ -28,14 +28,28 @@ func SourcePath() string {
 	return ""
 }
 
-// Subscribe this is a easy to use method to expose to objective-c and jave
+// Start accepts a callback and start the process of checking the version
+// and download and restart the
+func Start(callback Callback) {
+	// attach the following callback to all of the events
+	subscribe(NoUpdate, callback)
+	subscribe(UpdateAvailable, callback)
+	subscribe(UpdateDownloading, callback)
+	subscribe(UpdateDownloaded, callback)
+
+	go func() {
+
+	}()
+}
+
+// subscribe this is a easy to use method to expose to objective-c and jave
 // so they can bind their callbacks to known EventKinds
-func Subscribe(eventKind EventKind, callback Callback) {
+func subscribe(eventKind EventKind, callback Callback) {
 	conf.pubSub.Subscribe(eventKind, callback)
 }
 
-// Unsubscribe as it stands, it unsubscribes the any associate
+// unsubscribe as it stands, it unsubscribes the any associate
 // callback to specific event type. Mainly it's being used for clean up
-func Unsubscribe(eventKind EventKind) {
+func unsubscribe(eventKind EventKind) {
 	conf.pubSub.Unsubscribe(eventKind)
 }
