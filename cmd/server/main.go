582e62bae50633d06995f154c2bb3b14abf16219
Ali Najafizadeh <a.najafizadeh@gmail.com>
2017-05-26 14:38:01 -0400

    wip server upload component

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,18 +4,22 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 
 	context "golang.org/x/net/context"
 
 	"github.com/asdine/storm"
 	"github.com/asdine/storm/codec/protobuf"
 	"github.com/kelseyhightower/envconfig"
+	uuid "github.com/satori/go.uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
+	"github.com/pressly/warpdrive/helper"
 	pb "github.com/pressly/warpdrive/proto"
 )
 
@@ -67,6 +71,7 @@ func openDB(path string) (*storm.DB, error) {
 }
 
 type commandServer struct {
+	db *storm.DB
 }
 
 func (c *commandServer) CreateApp(context.Context, *pb.App) (*pb.App, error) {
@@ -89,10 +94,114 @@ func (c *commandServer) UpdateRelease(context.Context, *pb.Release) (*pb.Release
 }
 
 func (c *commandServer) UploadRelease(upload pb.Command_UploadReleaseServer) error {
-	return nil
+	var releaseID uint64
+	var total int64
+	var receivedBytes int64
+	var err error
+	var hash string
+	var chunck *pb.Chunck
+	var moved bool
+
+	filename := uuid.NewV4().String()
+	path := fmt.Sprintf("/tmp/%s", filename)
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		file.Close()
+		// if there is an error, the tmp file should be cleaned up
+		if err != nil {
+			if moved {
+				path = fmt.Sprintf("/bundles/%s", hash)
+			}
+
+			err = os.Remove(path)
+			if err != nil {
+				log.Println(err.Error())
+				log.Printf("hash '%s' value related to above \n", hash)
+			}
+		}
+	}()
+
+	for {
+		chunck, err = upload.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return err
+		}
+
+		header := chunck.GetHeader()
+		body := chunck.GetBody()
+
+		if header != nil {
+			if releaseID != 0 || total != 0 {
+				err = fmt.Errorf("chunck header sent multiple times")
+				return err
+			}
+			releaseID = header.ReleaseId
+			total = header.Total
+		} else if body != nil {
+			receivedBytes += int64(len(body.Data))
+			file.Write(body.Data)
+		}
+	}
+
+	if total == 0 || releaseID == 0 {
+		err = fmt.Errorf("header is not sent")
+		return err
+	}
+
+	if receivedBytes != total {
+		err = fmt.Errorf("the total amount is not matched")
+		return err
+	}
+
+	// calculate the hash value
+	hash, err = helper.HashFile(path)
+	if err != nil {
+		return err
+	}
+
+	release := pb.Release{}
+
+	err = c.db.One("id", releaseID, &release)
+	if err != nil {
+		return err
+	}
+
+	tx, err := c.db.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	release.Bundle = hash
+	err = tx.Save(release)
+	if err != nil {
+		return err
+	}
+
+	// move the file to bundles folder
+	err = os.Rename(path, fmt.Sprintf("/bundles/%s", hash))
+	if err != nil {
+		return err
+	}
+
+	// this is only to clean up the file either from tmp or bundles folder
+	moved = true
+
+	err = tx.Commit()
+	return err
 }
 
 type queryServer struct {
+	db *storm.DB
 }
 
 func (q *queryServer) GetUpgrade(context.Context, *pb.Upgrade) (*pb.Release, error) {
@@ -148,18 +257,18 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	_, err = openDB("/db/warpdrive.db")
+	db, err := openDB("/db/warpdrive.db")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	pb.RegisterCommandServer(grpcCommandServer, &commandServer{})
+	pb.RegisterCommandServer(grpcCommandServer, &commandServer{db})
 	lnCommand, err := net.Listen("tcp", fmt.Sprintf(":%s", commandEnv.Port))
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	pb.RegisterQueryServer(grpcQueryServer, &queryServer{})
+	pb.RegisterQueryServer(grpcQueryServer, &queryServer{db})
 	lnQuery, err := net.Listen("tcp", fmt.Sprintf(":%s", queryEnv.Port))
 	if err != nil {
 		log.Fatal(err.Error())
