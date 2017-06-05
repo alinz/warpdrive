1d4f9bb695f3dd03b3d41cfdab54894e0058b203
Ali Najafizadeh <a.najafizadeh@gmail.com>
2017-06-05 11:53:22 -0400

    android with example

--- client/android/src/main/java/com/pressly/warpdrive/WarpdrivePackage.java
+++ client/android/src/main/java/com/pressly/warpdrive/WarpdrivePackage.java
@@ -1,5 +1,145 @@
 package com.pressly.warpdrive;
 
-public class WarpdrivePackage {
-  
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import warpdrive.Warpdrive;
+
+public class WarpdrivePackage implements ReactPackage {
+
+    public WarpdrivePackage(Context context, String app, String rolloutAt, String serverAddr, String deviceCert, String deviceKey, String caCert) {
+        String documentPath = context.getFilesDir().getAbsolutePath();
+        String bundlePath = getBundlePath(context, documentPath, new String[]{ deviceCert, deviceKey, caCert });
+        String platform = "android";
+        String bundleVersion = getBundleVersion(context);
+
+        deviceCert = joinPath(bundlePath, deviceCert);
+        deviceKey = joinPath(bundlePath, deviceKey);
+        caCert = joinPath(bundlePath, caCert);
+
+        try {
+            Warpdrive.init(bundlePath, documentPath, platform, app, rolloutAt, bundleVersion, serverAddr, deviceCert, deviceKey, caCert);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String sourceBundle() {
+        String path = Warpdrive.bundlePath();
+        if (path == null || path.equals("")) {
+            path = null;
+        }
+        return path;
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new WarpdriveModule(reactContext));
+        return modules;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    // since we can't get the bundle path in android,
+    // we need to copy all certificate files into document folder under `warpdrive_bundle`
+    // this folder will keep overridden every time app boots up
+    private static String getBundlePath(Context context, String documentPath, String[] filenames) {
+        String bundlePath = joinPath(documentPath, "warpdrive_assets");
+
+        // create a warpdrive_assets folder
+        new File(bundlePath).mkdirs();
+
+        InputStream src = null;
+        OutputStream dst = null;
+
+        try {
+            for (String filename : filenames) {
+                src = context.getAssets().open(filename);
+                dst = new FileOutputStream(joinPath(bundlePath, filename));
+                if (copy(dst, src) == 0) {
+                    System.out.printf("couldn't copy %s", filename);
+                    break;
+                }
+                src.close();
+                dst.close();
+            }
+        } catch (IOException e) {
+            if (src != null) {
+                try {
+                    src.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+
+        return bundlePath;
+    }
+
+    private static String getBundleVersion(Context context) {
+        PackageManager manager = context.getPackageManager();
+        PackageInfo info = null;
+        try {
+            info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private static String joinPath(String a, String b) {
+        return new File(a, b).getPath();
+    }
+
+    private static int copy(OutputStream des, InputStream src) throws IOException {
+        byte[] buffer = new byte[1024]; // 1kb for now it's enough for us
+        int total = 0;
+
+        int read;
+        while ((read = src.read(buffer)) != -1) {
+            des.write(buffer, 0, read);
+            total += read;
+        }
+
+        return total;
+    }
+
+
+    class WarpdriveModule extends ReactContextBaseJavaModule {
+        public WarpdriveModule(final ReactApplicationContext reactContext) {
+            super(reactContext);
+        }
+
+        @Override
+        public String getName() {
+            return "WarpdriveManager";
+        }
+    }
 }
--- example/Sample/android/app/src/main/java/com/sample/MainApplication.java
+++ example/Sample/android/app/src/main/java/com/sample/MainApplication.java
@@ -1,12 +1,14 @@
 package com.sample;
 
 import android.app.Application;
+import android.support.annotation.Nullable;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.pressly.warpdrive.WarpdrivePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,9 +23,22 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
+      String app = "sample";
+      String rollout = "dev";
+      String serverAddr = "192.168.1.183:10001";
+      String deviceCert = "device.crt";
+      String deviceKey = "device.key";
+      String caCert = "ca.crt";
+
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new WarpdrivePackage(MainApplication.this, app, rollout, serverAddr, deviceCert, deviceKey, caCert)
       );
+    }
+
+    @Override
+    protected @Nullable String getJSBundleFile() {
+      return WarpdrivePackage.sourceBundle();
     }
   };
 
